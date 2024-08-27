347c666c952d4b545275be4c728475a16bc37e28
Reveny <113244907+reveny@users.noreply.github.com>
2024-08-27 16:47:53 +0200

    Update MainHook.java

--- app/src/main/java/com/reveny/protecttai/bypass/MainHook.java
+++ app/src/main/java/com/reveny/protecttai/bypass/MainHook.java
@@ -13,7 +13,7 @@ public class MainHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals("com.kotak.neo")) {
-            XposedHelpers.findAndHookMethod("f.g", lpparam.classLoader, "u1", String.class, int.class, int.class, int.class, String.class, int.class, new XC_MethodHook() {
+            XposedHelpers.findAndHookMethod("f.g", lpparam.classLoader, "t1", String.class, int.class, int.class, int.class, String.class, int.class, new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     Log.d("Xposed", "Hooked AppProtecttInteractor");
